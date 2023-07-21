7b0bc706a65acdc362571cf53eda718415f04d9a
Timur Mendybayev <mendybaev@gmail.com>
2023-07-21 10:11:15 +0300

    Commit 1

--- m16homework/ViewController.swift
+++ m16homework/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+//    class TableView: UITableView {
+//
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    
 }
 
